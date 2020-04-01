0908c68fedb686fec7d8313ffe51515ae6f9d478
tisyaputra <62689869+tisyaputra@users.noreply.github.com>
2020-04-01 13:49:22 +0700

    Add files via upload

---
+++ Praktikum1/src/classutama.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
+public class classutama {
+    public static void main(String[] args) {
+        //frameutama futama = new frameutama();
+        //futama.tampil("Program Pertama GUI");   
+        
+        futama utama =new futama();
+        //utama.setTitle("Pemerograman Provis");
+        utama.setVisible(true);
+    }
+    
+}
